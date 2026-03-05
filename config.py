9b90fd02117df6f17d953a5a5bccc14825e05154
soumyajit-cys <duttasoumyajit2005@gmail.com>
2026-03-06 01:25:43 +0530

    day 1

---
+++ config.py
@@ -0,0 +1,12 @@
+SCAN_DIRECTORIES = [
+    "/var/log",
+    "/home",
+    "/tmp",
+    "/etc"
+]
+
+DATABASE_PATH = "database/evidence.db"
+
+PCAP_FILE = "scans/network_capture.pcap"
+
+REPORT_PATH = "reports/forensic_report.html"
